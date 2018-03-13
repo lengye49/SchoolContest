6322c459fa028e2aac18733ea8f90df5d01112f1
snake <lib0707@gmail.com>
2018-03-13 10:10:44 +0800

    修复获取排名的信息错误的bug

--- Project C_pc/Assets/Scripts/Net/Common.cs
+++ Project C_pc/Assets/Scripts/Net/Common.cs
@@ -8,13 +8,17 @@ public class Common  {
 public enum RequestCode{
     None,
     Register,
+    Rank,
     Game,
 }
 
 public enum ActionCode{
     None,
     Register,
+    InitRanks,
+    SaveRanks,
     GetTotalRank,
+    GetAreaList,
     GetPersonalResult,
 }
 	
